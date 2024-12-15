d544f89650d8f12e8ebfd95d9040e658d0ee06a0
Luke aka SwissalpS <Luke@SwissalpS.ws>
2024-12-15 02:28:44 +0100

    move crafting recipes to own file

---
+++ crafts.lua
@@ -0,0 +1,163 @@
+-- TODO: xcompat: paper, stick, coal lump
+
+core.register_craft({
+	type = "shapeless",
+	output = "ehlphabet:block",
+	recipe = { "group:ehlphabet_block" },
+})
+
+-- blank blocks
+core.register_craft({
+	output = "ehlphabet:block 8",
+	recipe = {
+		{ "default:paper", "default:paper", "default:paper" },
+		{ "default:paper", "", "default:paper" },
+		{ "default:paper", "default:paper", "default:paper" },
+	},
+})
+
+-- build the machine!
+core.register_craft({
+	output = "ehlphabet:machine",
+	recipe = {
+		{ "default:stick", "default:coal_lump", "default:stick" },
+		{ "default:coal_lump", "ehlphabet:block", "default:coal_lump" },
+		{ "default:stick", "default:coal_lump", "default:stick" },
+	},
+})
+
+-- craft unused blocks back into paper
+core.register_craft({
+	output = "default:paper",
+	recipe = { "ehlphabet:block" },
+	type = "shapeless",
+})
+
+-- Chinese Characters - craft from latin characters
+core.register_craft({
+	output = "ehlphabet:231140 4",
+	recipe = {
+		{ "", "", "" },
+		{ "ehlphabet:78", "", "" },
+		{ "ehlphabet:69", "ehlphabet:75", "ehlphabet:79" },
+	},
+})
+
+core.register_craft({
+	output = "ehlphabet:229140 5",
+	recipe = {
+		{ "ehlphabet:78", "ehlphabet:79", "ehlphabet:82" },
+		{ "ehlphabet:84", "ehlphabet:72", "" },
+		{ "", "", "" },
+	},
+})
+
+core.register_craft({
+	output = "ehlphabet:228184 5",
+	recipe = {
+		{ "ehlphabet:69", "ehlphabet:65", "ehlphabet:83" },
+		{ "ehlphabet:84", "", "ehlphabet:83" },
+		{ "", "", "" },
+	},
+})
+
+core.register_craft({
+	output = "ehlphabet:230157 5",
+	recipe = {
+		{ "ehlphabet:69", "ehlphabet:65", "ehlphabet:83" },
+		{ "ehlphabet:84", "", "ehlphabet:84" },
+		{ "", "", "" },
+	},
+})
+
+core.register_craft({
+	output = "ehlphabet:229141 5",
+	recipe = {
+		{ "ehlphabet:83", "ehlphabet:79", "ehlphabet:85" },
+		{ "ehlphabet:84", "ehlphabet:72", "" },
+		{ "", "", "" },
+	},
+})
+
+core.register_craft({
+	output = "ehlphabet:232165 4",
+	recipe = {
+		{ "ehlphabet:87", "ehlphabet:69", "ehlphabet:83" },
+		{ "ehlphabet:84", "", "" },
+		{ "", "", "" },
+	},
+})
+
+core.register_craft({
+	output = "ehlphabet:231171 7",
+	recipe = {
+		{ "ehlphabet:83", "ehlphabet:84", "ehlphabet:65" },
+		{ "ehlphabet:84", "ehlphabet:73", "ehlphabet:79" },
+		{ "ehlphabet:78", "", "" },
+	},
+})
+
+-- stickers
+core.register_craft({
+	output = "ehlphabet:231140_sticker 4",
+	recipe = {
+		{ "", "", ""},
+		{ "ehlphabet:78_sticker", "", "" },
+		{ "ehlphabet:69_sticker", "ehlphabet:75_sticker", "ehlphabet:79_sticker" },
+	},
+})
+
+core.register_craft({
+	output = "ehlphabet:229140_sticker 5",
+	recipe = {
+		{ "ehlphabet:78_sticker", "ehlphabet:79_sticker", "ehlphabet:82_sticker" },
+		{ "ehlphabet:84_sticker", "ehlphabet:72_sticker", "" },
+		{ "", "", "" },
+	},
+})
+
+core.register_craft({
+	output = "ehlphabet:228184_sticker 5",
+	recipe = {
+		{ "ehlphabet:69_sticker", "ehlphabet:65_sticker", "ehlphabet:83_sticker" },
+		{ "ehlphabet:84_sticker", "", "ehlphabet:83_sticker" },
+		{ "", "", "" },
+	},
+})
+
+core.register_craft({
+	output = "ehlphabet:230157_sticker 5",
+	recipe = {
+		{ "ehlphabet:69_sticker", "ehlphabet:65_sticker", "ehlphabet:83_sticker" },
+		{ "ehlphabet:84_sticker", "", "ehlphabet:84_sticker" },
+		{ "", "", "" },
+	},
+})
+
+core.register_craft({
+	output = "ehlphabet:229141_sticker 5",
+	recipe = {
+		{ "ehlphabet:83_sticker", "ehlphabet:79_sticker", "ehlphabet:85_sticker" },
+		{ "ehlphabet:84_sticker", "ehlphabet:72_sticker", "" },
+		{ "", "", "" },
+	},
+})
+
+core.register_craft({
+	output = "ehlphabet:232165_sticker 4",
+	recipe = {
+		{ "ehlphabet:87_sticker", "ehlphabet:69_sticker", "ehlphabet:83_sticker" },
+		{ "ehlphabet:84_sticker", "", "" },
+		{ "", "", "" },
+	},
+})
+
+core.register_craft({
+	output = "ehlphabet:231171_sticker 7",
+	recipe = {
+		{ "ehlphabet:83_sticker", "ehlphabet:84_sticker", "ehlphabet:65_sticker" },
+		{ "ehlphabet:84_sticker", "ehlphabet:73_sticker", "ehlphabet:79_sticker" },
+		{ "ehlphabet:78_sticker", "", "" },
+	},
+})
+
--- init.lua
+++ init.lua
@@ -7,12 +7,17 @@ ehlphabet.path = core.get_modpath(core.get_current_modname())
 local S = dofile(ehlphabet.path .. "/intllib.lua")
 ehlphabet.intllib = S
 
+-- register crafting recipes
+dofile(ehlphabet.path .. "/crafts.lua")
+
+
 local function table_merge(t1, t2)
 	for _, v in ipairs(t2) do
 		table.insert(t1, v)
 	end
 	return t1
 end
+
 
 local digits = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" }
 local base_chars = {
@@ -165,7 +170,6 @@ end
 -- generate all available blocks and stickers
 generate(characters, add_to_guides)
 generate(additional_chars, true)
-minetest.register_craft({type = "shapeless", output = "ehlphabet:block", recipe = {"group:ehlphabet_block"}})
 
 -- blank node
 core.register_node("ehlphabet:block", {
@@ -299,161 +303,6 @@ core.register_node("ehlphabet:machine", {
 --  Alias  (Och_Noe 20180124)
 core.register_alias("abjphabet:machine", "ehlphabet:machine")
 --
-
---RECIPE: blank blocks
-minetest.register_craft({
-    output = "ehlphabet:block 8",
-    recipe = {
-        {"default:paper", "default:paper", "default:paper"},
-        {"default:paper", "", "default:paper"},
-        {"default:paper", "default:paper", "default:paper"}
-    }
-
---RECIPE: build the machine!
-minetest.register_craft({
-    output = "ehlphabet:machine",
-    recipe = {
-        {"default:stick", "default:coal_lump", "default:stick"},
-        {"default:coal_lump", "ehlphabet:block", "default:coal_lump"},
-        {"default:stick", "default:coal_lump", "default:stick"}
-    }
-})
-
---RECIPE: craft unused blocks back into paper
-minetest.register_craft({
-    output = "default:paper",
-    recipe = {"ehlphabet:block"},
-    type = "shapeless"
-})
-
--- Chinese Characters - craft from latin characters
-minetest.register_craft({
-    output = "ehlphabet:231140 4",
-    recipe = {
-        {"", "", ""},
-        {"ehlphabet:78", "", ""},
-        {"ehlphabet:69", "ehlphabet:75", "ehlphabet:79"}
-    }
-})
-
-minetest.register_craft({
-    output = "ehlphabet:229140 5",
-    recipe = {
-        {"ehlphabet:78", "ehlphabet:79", "ehlphabet:82"},
-        {"ehlphabet:84", "ehlphabet:72", ""},
-        {"", "", ""},
-    }
-})
-
-minetest.register_craft({
-    output = "ehlphabet:228184 5",
-    recipe = {
-        {"ehlphabet:69", "ehlphabet:65", "ehlphabet:83"},
-        {"ehlphabet:84", "", "ehlphabet:83"},
-        {"", "", ""},
-    }
-})
-
-minetest.register_craft({
-    output = "ehlphabet:230157 5",
-    recipe = {
-        {"ehlphabet:69", "ehlphabet:65", "ehlphabet:83"},
-        {"ehlphabet:84", "", "ehlphabet:84"},
-        {"", "", ""},
-    }
-})
-
-minetest.register_craft({
-    output = "ehlphabet:229141 5",
-    recipe = {
-        {"ehlphabet:83", "ehlphabet:79", "ehlphabet:85"},
-        {"ehlphabet:84", "ehlphabet:72", ""},
-        {"", "", ""},
-    }
-})
-
-minetest.register_craft({
-    output = "ehlphabet:232165 4",
-    recipe = {
-        {"ehlphabet:87", "ehlphabet:69", "ehlphabet:83"},
-        {"ehlphabet:84", "", ""},
-        {"", "", ""},
-    }
-})
-
-minetest.register_craft({
-    output = "ehlphabet:231171 7",
-    recipe = {
-        {"ehlphabet:83", "ehlphabet:84", "ehlphabet:65"},
-        {"ehlphabet:84", "ehlphabet:73", "ehlphabet:79"},
-        {"ehlphabet:78", "", ""},
-    }
-})
-
--- stickers
-minetest.register_craft({
-    output = "ehlphabet:231140_sticker 4",
-    recipe = {
-        {"", "", ""},
-        {"ehlphabet:78_sticker", "", ""},
-        {"ehlphabet:69_sticker", "ehlphabet:75_sticker", "ehlphabet:79_sticker"}
-    }
-})
-
-minetest.register_craft({
-    output = "ehlphabet:229140_sticker 5",
-    recipe = {
-        {"ehlphabet:78_sticker", "ehlphabet:79_sticker", "ehlphabet:82_sticker"},
-        {"ehlphabet:84_sticker", "ehlphabet:72_sticker", ""},
-        {"", "", ""},
-    }
-})
-
-minetest.register_craft({
-    output = "ehlphabet:228184_sticker 5",
-    recipe = {
-        {"ehlphabet:69_sticker", "ehlphabet:65_sticker", "ehlphabet:83_sticker"},
-        {"ehlphabet:84_sticker", "", "ehlphabet:83_sticker"},
-        {"", "", ""},
-    }
-})
-
-minetest.register_craft({
-    output = "ehlphabet:230157_sticker 5",
-    recipe = {
-        {"ehlphabet:69_sticker", "ehlphabet:65_sticker", "ehlphabet:83_sticker"},
-        {"ehlphabet:84_sticker", "", "ehlphabet:84_sticker"},
-        {"", "", ""},
-    }
-})
-
-minetest.register_craft({
-    output = "ehlphabet:229141_sticker 5",
-    recipe = {
-        {"ehlphabet:83_sticker", "ehlphabet:79_sticker", "ehlphabet:85_sticker"},
-        {"ehlphabet:84_sticker", "ehlphabet:72_sticker", ""},
-        {"", "", ""},
-    }
-})
-
-minetest.register_craft({
-    output = "ehlphabet:232165_sticker 4",
-    recipe = {
-        {"ehlphabet:87_sticker", "ehlphabet:69_sticker", "ehlphabet:83_sticker"},
-        {"ehlphabet:84_sticker", "", ""},
-        {"", "", ""},
-    }
-})
-
-minetest.register_craft({
-    output = "ehlphabet:231171_sticker 7",
-    recipe = {
-        {"ehlphabet:83_sticker", "ehlphabet:84_sticker", "ehlphabet:65_sticker"},
-        {"ehlphabet:84_sticker", "ehlphabet:73_sticker", "ehlphabet:79_sticker"},
-        {"ehlphabet:78_sticker", "", ""},
-    }
-})
-
 
 
 
